9bb8c865c51de39badb43c2a866cbbf19666429c
Muhamed Curri <muhamedcurri1@gmail.com>
2024-12-02 17:14:45 +0300

    add custom exceptions

---
+++ BookNest/Utils/CustomException.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Net;
+
+namespace BookNest.Utils
+{
+    public class CustomException : Exception
+    {
+        private readonly HttpStatusCode ErrorCode;
+        public CustomException(string message) : base(message) { }
+        public CustomException(HttpStatusCode errorCode, string message) : base(message) {
+            ErrorCode = errorCode;
+        }
+    }
+
+    public class ForbiddenException : CustomException
+    {
+        public ForbiddenException(string message) : base( HttpStatusCode.Forbidden,message) { }
+    }
+
+    public class NotFoundException : CustomException
+    {
+        public NotFoundException(string message) : base(HttpStatusCode.NotFound, message) { }
+    }
+
+    public class UnauthorizedException : CustomException
+    {
+        public UnauthorizedException(string message) : base(HttpStatusCode.Unauthorized, message) { }
+    }
+
+    public class ValidationException : CustomException
+    {
+        public ValidationException(string message) : base(HttpStatusCode.BadRequest, message) { }
+    }
+}

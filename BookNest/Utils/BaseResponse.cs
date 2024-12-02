758c7afaa43af8d9880825f1b5c05ebb45c50c08
Muhamed Curri <muhamedcurri1@gmail.com>
2024-12-02 17:17:49 +0300

    update baseresponse

--- BookNest/Utils/BaseResponse.cs
+++ BookNest/Utils/BaseResponse.cs
@@ -1,20 +1,36 @@
-﻿namespace BookNest.Utils
+﻿using System.Net;
+
+namespace BookNest.Utils
 {
-    public class BaseResponse
+    public class BaseResponse<T> : IBaseResponse
     {
         public bool Success { get; set; }
-        public string Message { get; set; }
-        public int Status {  get; set; }
+        public string? Message { get; set; }
+        public HttpStatusCode Status { get; set; }
+        public T? Body { get; set; }
+
+        object IBaseResponse.Body => Body;
 
         public BaseResponse() { }
-        public BaseResponse(bool success, string message, int status) { Success = success; Message = message; }
-        public static BaseResponse SuccessResponse(string message)
+
+        public BaseResponse(bool success,HttpStatusCode status, string message, T body)
         {
-            return new BaseResponse(true, message, 200);
+            Success = success;
+            Message = message;
+            Body = body;
+            Status = status;
         }
-        public static BaseResponse Error(string message, int status)
+
+        // Factory method for a success response
+        public static BaseResponse<T> SuccessResponse(T body)
         {
-            return new BaseResponse(false, message, status);
-        } 
+            return new BaseResponse<T>(true,HttpStatusCode.OK, null, body);
+        }
+
+        // Factory method for an error response
+        public static BaseResponse<T> ErrorResponse(HttpStatusCode status, string message)
+        {
+            return new BaseResponse<T>(false,HttpStatusCode.BadRequest, message, default(T)); // Body will be null or default(T) for errors
+        }
     }
 }
---
+++ BookNest/Utils/IBaseResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace BookNest.Utils
+{
+    public interface IBaseResponse
+    {
+        bool Success { get; set; }
+        string? Message { get; set; }
+        object? Body { get; }
+        HttpStatusCode Status { get; set; }
+    }
+}

2292879a18219522294ec8c1aed883bec482aa6f
Muhamed Curri <muhamedcurri1@gmail.com>
2024-12-24 01:17:55 +0300

    create challengedto

---
+++ BookNest/Dtos/Challenge/ChallengeDto.cs
@@ -0,0 +1,20 @@
+﻿using BookNest.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookNest.Dtos.Challenge
+{
+    public class ChallengeDto
+    {
+        public required string Text { get; set; }
+        [Required]
+        public required string Type { get; set; }
+        [Required]
+        public required int Objective { get; set; }
+        [Required]
+        public required bool IsCompleted { get; set; }
+        [Required]
+        public DateTime StartedAt { get; set; } = DateTime.Now;
+        [Required]
+        public DateTime EndsAt { get; set; }
+    }
+}

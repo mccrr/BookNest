186f77bb3ffb45075873c83f221d8fb43eaf6035
Muhamed Curri <muhamedcurri1@gmail.com>
2025-01-03 12:56:54 +0300

    update profiledto

--- BookNest/Dtos/Users/ProfileDto.cs
+++ BookNest/Dtos/Users/ProfileDto.cs
@@ -15,17 +15,6 @@ namespace BookNest.Dtos.Users
             Age = user.Age;
             Email = user.Email;
             Avatar = user.Avatar;
-            BookUsers = user.BookUsers;
-            UserAchievements = user.UserAchievements;
-            UserNotifications = user.UserNotifications;
-            SentFriendRequests = user.SentFriendRequests;
-            ReceivedFriendRequests = user.ReceivedFriendRequests;
-            GroupRequests = user.GroupRequests;
-            GroupUsers = user.GroupUsers;
-            FriendsInitiated = user.FriendsInitiated;
-            FriendsReceived = user.FriendsReceived;
-            Muted = user.Muted;
-            Muter = user.Muter;
         }
         public int Id { get; set; }
         public string Username { get; set; }
@@ -34,17 +23,5 @@ namespace BookNest.Dtos.Users
         public int Age { get; set; }
         public string Email { get; set; }
         public string Avatar { get; set; }
-
-        public ICollection<BookUser>? BookUsers { get; set; }
-        public ICollection<UserAchievement> UserAchievements { get; set; }
-        public ICollection<UserNotification> UserNotifications { get; set; }
-        public ICollection<FriendRequest> SentFriendRequests { get; set; }
-        public ICollection<FriendRequest> ReceivedFriendRequests { get; set; }
-        public ICollection<GroupRequest> GroupRequests { get; set; }
-        public ICollection<GroupUser> GroupUsers { get; set; }
-        public ICollection<Friend> FriendsInitiated { get; set; }
-        public ICollection<Friend> FriendsReceived { get; set; }
-        public ICollection<Mute> Muted { get; set; }
-        public ICollection<Mute> Muter { get; set; }
     }
 }

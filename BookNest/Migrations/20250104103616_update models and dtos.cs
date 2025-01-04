7b7591f1b55aeeb9e978c228029497c9745ba620
Muhamed Curri <muhamedcurri1@gmail.com>
2025-01-04 13:45:52 +0300

    add migrations

---
+++ BookNest/Migrations/20250104103616_update models and dtos.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookNest.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatemodelsanddtos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Rating",
+                table: "Reviews",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Rating",
+                table: "Reviews",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "float");
+        }
+    }
+}
